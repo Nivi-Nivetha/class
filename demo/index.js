d94810d8ed397e899fc356270841320032181796
Nivetha G <161429269+Nivi-Nivetha@users.noreply.github.com>
2024-02-27 15:58:52 +0530

    Add files via upload

---
+++ demo/index.js
@@ -0,0 +1,20 @@
+
+alert("who is this");
+
+const signInLink = document.querySelector(".signInBtn-link");
+const signUpLink = document.querySelector(".signUpBtn-link");
+
+const wrapper = document.querySelector(".wrapper");
+
+signUpLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+
+signInLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+
+
+
+
+
